eccf371683f6542f7ec3345d24d35407a92b9c78
SungKyuLee <cnusungkyu@o.cnu.ac.kr>
2023-04-09 03:03:59 +0900

    프로젝트글을 저장할 수 있다

---
+++ src/main/java/com/cnu/real_coding_server/controller/ProjectController.java
@@ -0,0 +1,23 @@
+package com.cnu.real_coding_server.controller;
+
+import com.cnu.real_coding_server.entity.Project;
+import com.cnu.real_coding_server.model.request.ProjectRequest;
+import com.cnu.real_coding_server.service.ProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/projects")
+public class ProjectController {
+    private final ProjectService projectService;
+
+    @PostMapping
+    public ResponseEntity<Project> createProject(@RequestBody ProjectRequest projectRequest) {
+        return ResponseEntity.ok(projectService.createProject(projectRequest));
+    }
+}
---
+++ src/main/java/com/cnu/real_coding_server/service/ProjectService.java
@@ -0,0 +1,17 @@
+package com.cnu.real_coding_server.service;
+
+import com.cnu.real_coding_server.entity.Project;
+import com.cnu.real_coding_server.model.request.ProjectRequest;
+import com.cnu.real_coding_server.repository.ProjectRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProjectService {
+    private final ProjectRepository projectRepository;
+
+    public Project createProject(ProjectRequest projectRequest) {
+        return projectRepository.save(projectRequest.toEntity());
+    }
+}
